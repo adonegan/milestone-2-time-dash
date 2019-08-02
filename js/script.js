b0247ae088e71d7a1a9d8ff5bd875581a0a4cb7e
adonegan <48014273+adonegan@users.noreply.github.com>
2019-08-02 14:29:15 +0000

    added print_filter and testing new bar chart

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@ queue()
     .defer(d3.csv, "data/archive2.csv")
     .await(makeGraphs);
 
+function print_filter(filter) {
+    var f=eval(filter);
+    if (typeof(f.length) != "undefined") {}else{}
+    if (typeof(f.top) != "undefined") {f=f.top(Infinity);}else{}
+    if (typeof(f.dimension) != "undefined") {f=f.dimension(function(d) { return "";}).top(Infinity);}else{}
+    console.log(filter+"("+f.length+") = "+JSON.stringify(f).replace("[","[\n\t").replace(/}\,/g,"},\n\t").replace("]","\n]"));
+}
+
 
 function makeGraphs(error, archiveData) {
     var ndx = crossfilter(archiveData);
@@ -10,6 +18,7 @@ function makeGraphs(error, archiveData) {
     show_country_pie(ndx);
     show_category_pie(ndx);
     show_honor_pie(ndx);
+    show_category_bar(ndx);
 
     dc.renderAll();
 
@@ -26,6 +35,8 @@ function show_country_pie(ndx) {
         .width(300)
         .radius(200)
         .transitionDuration(1500);
+        
+        // print_filter(group);
 }
 
 function show_category_pie(ndx) {
@@ -39,6 +50,8 @@ function show_category_pie(ndx) {
         .width(300)
         .radius(200)
         .transitionDuration(1500);
+        
+        //print_filter(group);
         
 }
 
@@ -54,4 +67,22 @@ function show_honor_pie(ndx) {
         .radius(200)
         .transitionDuration(1500);
         
+        print_filter(group);
+        
 }
+
+function show_category_bar(ndx) {
+    var categoryDimension = ndx.dimension(function(d){ return d.category});
+    var categoryGroup = categoryDimension.group();
+    
+   dc.barChart("#bar-chart")
+        .dimension(categoryDimension)
+        .group(categoryGroup)
+        .x(d3.scale.ordinal().domain(["Politics", "Economics"]))
+        .xUnits(dc.units.ordinal);
+        
+  // print_filter(categoryDimension);
+  // console.log(categoryDimension);     
+
+}
+
