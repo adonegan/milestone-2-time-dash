49792028afcffbb94d13249e79b2f0f0e0d1d915
adonegan <48014273+adonegan@users.noreply.github.com>
2019-08-15 20:41:36 +0000

    added jQuery slideToggle boxes to hide instructions

--- js/script.js
+++ js/script.js
@@ -201,3 +201,43 @@ $(document).ready(function(){
   });
 });
 
+ // W3Schools code 
+
+$(document).ready(function(){
+  $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+  });
+});
+
+
+ // W3Schools code 
+
+$(document).ready(function(){
+  $("#flip-1").click(function(){
+    $("#panel-1").slideToggle("slow");
+  });
+});
+
+ // W3Schools code 
+
+$(document).ready(function(){
+  $("#flip-2").click(function(){
+    $("#panel-2").slideToggle("slow");
+  });
+});
+
+
+
+$(document).ready(function(){
+  $("#flip-3").click(function(){
+    $("#panel-3").slideToggle("slow");
+  });
+});
+
+
+
+$(document).ready(function(){
+  $("#flip-4").click(function(){
+    $("#panel-4").slideToggle("slow");
+  });
+});
